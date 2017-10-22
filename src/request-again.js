c80d4759639844ffd56486a191cbb1ac7142a05e
kobi.carmeli <kobi.carmeli@zooz.com>
2017-10-22 15:05:11 +0300

    Added jsdoc templates

--- src/request-again.js
+++ src/request-again.js
@@ -7,55 +7,37 @@ let CustomError = require('./CustomError');
 let defaults = require('lodash').defaults;
 let isObject = require('lodash').isObject;
 
-module.exports = function sendRequest(request, method, uri, options) {
-    let [_uri, _options, retryOptions] = getOptions(uri, options, request.retryDefaults);
+/**
+ * Send request-again request
+ *
+ * @param {object} request the request-promise instance to use
+ * @param {string} method the method to send
+ * @param {any} uri the url to send the request to or options
+ * @param {object} options
+ * @returns
+ */
+module.exports = function requestAgain(request, method, uri, options) {
+    let [_uri, _options, retryOptions] = buildOptions(uri, options, request.retryDefaults);
 
     let errors = [];
     return retry(() => {
         return request[method](_uri, _options)
-            .then((response) => {
-                return handleResponse(retryOptions, response);
-            })
-            .catch((error) => {
-                errors.push(error);
-                if (retryOptions.logFn) {
-                    retryOptions.logFn(retryOptions, error, errors.length);
-                }
-                throw error;
-            });
+            .then((response) => handleResponse(retryOptions, response))
+            .catch((error) => handleError(retryOptions, error, errors));
     }, retryOptions)
         .then((response) => buildResponse(response, errors))
         .catch((error) => { throw buildErrorResponse(error, errors) });
 };
 
-function handleResponse(retryOptions, response) {
-    let {retryOn5xx, retryStrategyFn} = retryOptions;
-
-    if (retryOn5xx && response.statusCode >= 500) {
-        throw new StatusCodeError(response);
-    } else if (typeof retryStrategyFn === 'function' && retryStrategyFn(response)) {
-        throw new CustomError(response);
-    } else {
-        return response;
-    }
-}
-
-function buildResponse(response, errors) {
-    if (isObject(response)) {
-        response.errors = map(errors, (error) => error.message);
-        response.retries = response.errors.length;
-        return response.body;
-    } else {
-        return response;
-    }
-}
-
-function buildErrorResponse(error, errors) {
-    error.retries = errors.length;
-    return error;
-}
-
-function getOptions(url, options, retryDefaults) {
+/**
+ *
+ *
+ * @param {string} url
+ * @param {object} options
+ * @param {object} retryDefaults
+ * @returns
+ */
+function buildOptions(url, options, retryDefaults) {
     let retryOptions;
     if (typeof url === 'string') {
         options = options || {};
@@ -69,9 +51,78 @@ function getOptions(url, options, retryDefaults) {
     return [url, options, retryOptions];
 }
 
+/**
+ *
+ *
+ * @param {object} requestOptions
+ * @param {object} retryOptions
+ */
 function buildRequestOptions(requestOptions, retryOptions) {
     if (retryOptions.retryOn5xx === true || typeof retryOptions.retryStrategyFn === 'function') {
         requestOptions.simple = false;
         requestOptions.resolveWithFullResponse = true;
     }
+}
+
+/**
+ *
+ *
+ * @param {object} retryOptions
+ * @param {object} response
+ * @returns
+ */
+function handleResponse(retryOptions, response) {
+    let {retryOn5xx, retryStrategyFn} = retryOptions;
+
+    if (retryOn5xx && response.statusCode >= 500) {
+        throw new StatusCodeError(response);
+    } else if (typeof retryStrategyFn === 'function' && retryStrategyFn(response)) {
+        throw new CustomError(response);
+    } else {
+        return response;
+    }
+}
+
+/**
+ *
+ *
+ * @param {object} retryOptions
+ * @param {Error} error
+ * @param {array} errors
+ */
+function handleError(retryOptions, error, errors) {
+    errors.push(error);
+    if (retryOptions.logFn) {
+        retryOptions.logFn(error, errors.length);
+    }
+    throw error;
+}
+
+/**
+ *
+ *
+ * @param {object} response
+ * @param {array} errors
+ * @returns
+ */
+function buildResponse(response, errors) {
+    if (isObject(response)) {
+        response.errors = map(errors, (error) => error.message);
+        response.retries = response.errors.length;
+        return response.body;
+    } else {
+        return response;
+    }
+}
+
+/**
+ *
+ *
+ * @param {Error} error
+ * @param {array} errors
+ * @returns
+ */
+function buildErrorResponse(error, errors) {
+    error.retries = errors.length;
+    return error;
 }
--- test.js
+++ test.js
@@ -4,14 +4,17 @@ var test = ra.defaults({
     retry: {
         max: 3,
         retryStrategyFn: function (response) {
-            console.info(response.statusCode);
             return response.statusCode === 405;
+        },
+        logFn: function (error, count) {
+            console.info('--------');
+            console.info(error, count);
         }
     }
 });
 var test2 = ra.defaults();
 // ra.post('http://www.google.com')
-test2.post('http://www.google.com')
+test.post('http://www.google.com')
 // test.post('http://www.google.com', {retry: {max: 2}})
 // test.post({url: 'http://www.google.com', retry: {max: 2}})
     .then((res) => {
